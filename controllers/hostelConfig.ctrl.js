74abbe24991ded70f6d4363f72d4006a8c314134
SandipBehera <sandip.sudip36@gmail.com>
2023-12-11 09:29:47 +0530

    fixed the hostelconfig

--- controllers/hostelConfig.ctrl.js
+++ controllers/hostelConfig.ctrl.js
@@ -20,32 +20,91 @@ exports.getAllConfigs = (req, res) => {
 exports.addConfig = (req, res) => {
   const { config_type, config_type_name, branch_id } = req.body;
 
+  // Check if the record already exists
   connection.query(
-    `INSERT INTO hostel_config (
-        config_type,
-        config_type_name,
-        branch_id
-    ) VALUES (
-        '${config_type}',
-        '${config_type_name}',
-        '${branch_id}'
-    ) ON DUPLICATE KEY UPDATE
-        config_type_name = JSON_MERGE_PRESERVE(
-            IFNULL(config_type_name, JSON_ARRAY()),
-            VALUES(config_type_name)
-        )`,
-    (err, result) => {
-      if (err) {
-        logger.error(err);
+    "SELECT * FROM hostel_config WHERE config_type = ? AND branch_id = ?",
+    [config_type, branch_id],
+    (selectErr, selectResult) => {
+      if (selectErr) {
+        logger.error(selectErr);
+        res
+          .status(500)
+          .send({ message: "Internal Server Error", status: "error" });
+        return;
       }
-      if (result) {
-        res.send({
-          data: result,
-          message: `${config_type} Config Added`,
-          status: "success",
-        });
+
+      if (selectResult.length === 0) {
+        // Record doesn't exist, perform an INSERT
+        connection.query(
+          "INSERT INTO hostel_config (config_type, config_type_name, branch_id) VALUES (?, ?, ?)",
+          [config_type, config_type_name, branch_id],
+          (insertErr, insertResult) => {
+            if (insertErr) {
+              logger.error(insertErr);
+              res
+                .status(500)
+                .send({ message: "Internal Server Error", status: "error" });
+              return;
+            }
+            res.send({
+              data: insertResult,
+              message: `${config_type} Config Added`,
+              status: "success",
+            });
+          }
+        );
       } else {
-        res.send({ message: "No Config Added", staus: "error" });
+        // Record exists, update config_type_name by appending new values
+        const existingConfig = selectResult[0];
+
+        // Log existing values
+        console.log(
+          "existingConfig.config_type_name:",
+          existingConfig.config_type_name
+        );
+        console.log("config_type_name:", config_type_name);
+
+        // Try parsing JSON
+        try {
+          const existingDataArray =
+            existingConfig.config_type_name.data || "[]";
+          const newDataArray = JSON.parse(config_type_name.data || "[]");
+
+          const updatedDataArray = [
+            ...(existingConfig.config_type_name.data || []),
+            ...(JSON.parse(config_type_name).data || []),
+          ];
+
+          console.log(updatedDataArray);
+          connection.query(
+            "UPDATE hostel_config SET config_type_name = ? WHERE config_type = ? AND branch_id = ?",
+            [
+              JSON.stringify({ data: updatedDataArray }),
+              config_type,
+              branch_id,
+            ],
+            (updateErr, updateResult) => {
+              if (updateErr) {
+                logger.error(updateErr);
+                res
+                  .status(500)
+                  .send({ message: "Internal Server Error", status: "error" });
+                return;
+              }
+              res.send({
+                data: updateResult,
+                message: `${config_type} Config Updated`,
+                status: "success",
+              });
+            }
+          );
+        } catch (jsonParseError) {
+          // Log JSON parsing error
+          console.error("JSON Parsing Error:", jsonParseError);
+          res
+            .status(400)
+            .send({ message: "Invalid JSON data", status: "error" });
+        }
       }
     }
   );
