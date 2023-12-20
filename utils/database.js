0a8d2245770dadbb8cb371084067e8814c0ee547
SandipBehera <sandip.sudip36@gmail.com>
2023-12-21 04:56:15 +0530

    added Handle disconnection error

--- utils/database.js
+++ utils/database.js
@@ -10,5 +10,14 @@ connection.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
 });
-
+// Handle disconnection error
+connection.on("error", (err) => {
+  console.error("Database connection error:", err);
+  if (err.code === "PROTOCOL_CONNECTION_LOST") {
+    console.error("Database connection was closed. Reconnecting...");
+    // You can attempt to reconnect here if needed
+  } else {
+    throw err;
+  }
+});
 module.exports = connection;
