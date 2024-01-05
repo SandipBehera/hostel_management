6b3b5780d548e4c331a432f9135b8dc02ba4dfb9
SandipBehera <sandip.sudip36@gmail.com>
2024-01-05 10:39:58 +0530

    added auth

--- controllers/user.ctrl.js
+++ controllers/user.ctrl.js
@@ -33,8 +33,9 @@ exports.login = (req, res) => {
 };
 exports.web_login = (req, res) => {
   console.log(req.body);
-  const { user_id, date, userType, is_logged_in } = req.body;
+  const { user_id, date, userType, is_logged_in, Auth } = req.body;
   console.log(user_id);
+  logger.info(Auth);
   if (userType === "student") {
     connection.query(
       `SELECT users.*, user_room_assign.room_id,rooms.hostel_name
